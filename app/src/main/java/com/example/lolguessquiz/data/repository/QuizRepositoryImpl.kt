af366d66dbaa7dd9bd604ffaaa280d57e9e9fd67
Sreten <banovicbl22@gmail.com>
2022-12-13 15:40:39 +0100

    Quiz repository interface

---
+++ app/src/main/java/com/example/lolguessquiz/data/repository/QuizRepositoryImpl.kt
@@ -0,0 +1,4 @@
+package com.example.lolguessquiz.data.repository
+
+class QuizRepositoryImpl  {
+}
--- app/src/main/java/com/example/lolguessquiz/domain/repository/QuizRepository.kt
+++ app/src/main/java/com/example/lolguessquiz/domain/repository/QuizRepository.kt
@@ -1,10 +1,14 @@
 package com.example.lolguessquiz.domain.repository
 
-import com.example.lolguessquiz.data.local.ScoreModelEntity
+import com.example.lolguessquiz.domain.model.ScoreModel
 import com.example.lolguessquiz.util.Resource
 import kotlinx.coroutines.flow.Flow
 
 interface QuizRepository {
 
-    suspend fun getScores(): Flow<Resource<ScoreModelEntity>>
+    suspend fun getScores(): Flow<Resource<ScoreModel>>
+    suspend fun getChampionPicture(): Flow<Resource<String>>
+    suspend fun getChampionPassive(): Flow<Resource<String>>
+    suspend fun getChampionSpell(): Flow<Resource<String>>
+
 }
