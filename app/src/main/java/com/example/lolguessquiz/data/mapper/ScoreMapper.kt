079e99a2d26e3a2808b73c8c1eaed31ae4915827
Sreten <banovicbl22@gmail.com>
2022-12-13 15:28:50 +0100

    ScoreMapper

--- app/src/main/java/com/example/lolguessquiz/data/mapper/ScoreMapper.kt
+++ app/src/main/java/com/example/lolguessquiz/data/mapper/ScoreMapper.kt
@@ -1,2 +1,22 @@
 package com.example.lolguessquiz.data.mapper
 
+import com.example.lolguessquiz.data.local.ScoreModelEntity
+import com.example.lolguessquiz.domain.model.ScoreModel
+
+fun ScoreModelEntity.toScoreModel(): ScoreModel{
+    return ScoreModel(
+        maxScoreGuessChampion = maxScoreGuessChampion,
+        maxScoreGuessPassive = maxScoreGuessPassive,
+        maxScoreGuessSpellName = maxScoreGuessSpellName,
+        maxScoreGuessChampionFromSpell = maxScoreGuessChampionFromSpell,
+    )
+}
+
+fun ScoreModel.toScoreModelEntity(): ScoreModelEntity{
+    return ScoreModelEntity(
+        maxScoreGuessChampion = maxScoreGuessChampion,
+        maxScoreGuessPassive = maxScoreGuessPassive,
+        maxScoreGuessSpellName = maxScoreGuessSpellName,
+        maxScoreGuessChampionFromSpell = maxScoreGuessChampionFromSpell,
+    )
+}
