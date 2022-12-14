5200363ff322dac266de22abf0549e7f3df9835b
Sreten <banovicbl22@gmail.com>
2022-12-14 13:50:12 +0100

    Row compose
    Making title and button for starting a quiz.

---
+++ app/src/main/java/com/example/lolguessquiz/presentation/main_screen/MainScreen.kt
@@ -0,0 +1,10 @@
+package com.example.lolguessquiz.presentation.main_screen
+
+import androidx.compose.runtime.Composable
+import com.ramcosta.composedestinations.annotation.Destination
+
+@Composable
+@Destination(start = true)
+fun MainScreen() {
+
+}
---
+++ app/src/main/java/com/example/lolguessquiz/presentation/main_screen/Row.kt
@@ -0,0 +1,63 @@
+package com.example.lolguessquiz.presentation.main_screen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowForward
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun RowQuiz(
+    nameOfQuiz: String,
+    modifier: Modifier = Modifier,
+    maxResult: Int,
+) {
+    Row(
+        modifier = modifier
+            .padding(start = 16.dp, end = 16.dp)
+            .fillMaxWidth()
+            .height(56.dp)
+    ){
+        Box(
+            modifier = modifier
+                .padding(top = 4.dp, bottom = 4.dp)
+                .size(48.dp)
+                .clip(CircleShape)
+                .background(Color.LightGray)
+        )
+        Spacer(modifier = modifier.width(8.dp))
+        Text(
+            text = nameOfQuiz,
+            modifier = modifier
+                .padding(top = 4.dp, bottom = 4.dp),
+            textAlign = TextAlign.Center,
+            fontSize = 12.sp,
+            fontFamily = FontFamily.SansSerif
+        )
+        Spacer(modifier = modifier.width(8.dp))
+        IconButton(
+            modifier = modifier.padding(end = 8.dp),
+            onClick = {
+                //
+            }
+        ) {
+            Icon(
+                imageVector = Icons.Rounded.ArrowForward,
+                contentDescription = null,
+            )
+        }
+    }
+}
